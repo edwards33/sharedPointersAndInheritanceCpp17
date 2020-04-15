637bbdd1a62705a174186f82fe9a3797c13c4d73
Edward <ewalbach@gmail.com>
2020-04-15 20:04:56 +0300

    init

---
+++ Tweeter.h
@@ -0,0 +1,19 @@
+#pragma once
+#include "Person.h"
+#include <string>
+
+class Tweeter :
+	public Person
+{
+private:
+	std::string twitterhandle;
+public:
+	Tweeter(std::string first,
+		std::string last,
+		int arbitrary,
+		std::string handle);
+	~Tweeter(void);
+	std::string GetName() const;
+	int GetNumber() const { return 0; }
+};
+
